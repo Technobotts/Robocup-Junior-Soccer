641a712270e17fc078423d2684fa34bbc17a8195
Eric <Eric@57c2b024-eea2-457c-b231-805d8debf23e>
2010-02-18 16:26:19 +0000

    Removed unnecessary code
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@35 57c2b024-eea2-457c-b231-805d8debf23e

--- Robocup/Rescue/trunk/Rescue with Calibration/Calibration/ColorView.java
+++ Robocup/Rescue/trunk/Rescue with Calibration/Calibration/ColorView.java
@@ -4,11 +4,8 @@ import javax.microedition.lcdui.Graphics;
 
 import technobotts.rescue.RawColor;
 
-import lejos.nxt.LCD;
-import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.addon.ColorSensor;
-import lejos.nxt.addon.LineLeader;
 
 
 public class ColorView
@@ -32,23 +29,14 @@ public class ColorView
 	public static void main(String... args) throws InterruptedException
 	{
 		ColorSensor cs = new ColorSensor(SensorPort.S1);
-		//LightSensor ls = new LightSensor(SensorPort.S1);
 		Graphics graphics = new Graphics();
 		graphics.autoRefresh(false);
 		
 		while(true)
 		{ 
 			graphics.clear();
-			//graphics.fillRect(0, 0, (int) (1/11.0), 63);
 			RawColor color = RawColor.fromHTSensor(cs);
 			drawColor(new Rectangle(10, 10, 80, 32),color);
-//			int r = (int) (Math.pow(color.getR(),1/3.0)*80/maxScaled);
-//			int g = (int) (Math.pow(color.getG(),1/3.0)*80/maxScaled);
-//			int b = (int) (Math.pow(color.getB(),1/3.0)*80/maxScaled);
-//			graphics.fillRect(0, 60, r, 2);
-//			graphics.fillRect(0, 57, g, 2);
-//			graphics.fillRect(0, 54, b, 2);
-//			graphics.refresh();
 			Thread.sleep(50);
 		}
 	}
