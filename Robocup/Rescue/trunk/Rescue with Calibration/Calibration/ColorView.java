2f937de4049fe395ab3b2df0c718c7df755b4d40
Eric Wieser <wieser.eric@gmail.com>
2010-02-18 12:05:53 +0000

    Updated to draw RGB bars within a given java.awt.Rectangle
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@31 57c2b024-eea2-457c-b231-805d8debf23e

--- Robocup/Rescue/trunk/Rescue with Calibration/Calibration/ColorView.java
+++ Robocup/Rescue/trunk/Rescue with Calibration/Calibration/ColorView.java
@@ -1,3 +1,5 @@
+import java.awt.Rectangle;
+
 import javax.microedition.lcdui.Graphics;
 
 import technobotts.rescue.RawColor;
@@ -11,24 +13,42 @@ import lejos.nxt.addon.LineLeader;
 
 public class ColorView
 {
+	public static void drawColor(Rectangle rect,RawColor color)
+	{
+		final double power = 5/12.0;
+		
+		Graphics graphics = new Graphics();
+		final double scale = rect.width/Math.pow(1024,power);
+		int r = (int) (Math.pow(color.getR()-1,power)*scale);
+		int g = (int) (Math.pow(color.getG()-1,power)*scale);
+		int b = (int) (Math.pow(color.getB()-1,power)*scale);
+		int height = (rect.height-1)/3;
+		graphics.fillRect(rect.x, rect.y, r, height-1);
+		graphics.fillRect(rect.x, rect.y+height, g, height-1);
+		graphics.fillRect(rect.x, rect.y+height*2, b, height-1);
+		graphics.refresh();
+	}
+	
 	public static void main(String... args) throws InterruptedException
 	{
-		//ColorSensor cs = new ColorSensor(SensorPort.S2);
-		LightSensor ls = new LightSensor(SensorPort.S1);
+		ColorSensor cs = new ColorSensor(SensorPort.S1);
+		//LightSensor ls = new LightSensor(SensorPort.S1);
 		Graphics graphics = new Graphics();
 		graphics.autoRefresh(false);
+		
 		while(true)
-		{
+		{ 
 			graphics.clear();
-			graphics.fillRect(0, 0, (int) (/11.0), 63);
-			//RawColor color = RawColor.fromHTSensor(cs);
-			//int r = (int) (Math.log(color.getR()/1008) / Math.log(2) * 80);
-			//int g = (int) (Math.log(color.getR()/1008) / Math.log(2) * 80);
-			//int b = (int) (Math.log(color.getR()/1008) / Math.log(2) * 80);
-			//graphics.fillRect(0, 60, r, 2);
-			//graphics.fillRect(0, 57, g, 2);
-			//graphics.fillRect(0, 54, b, 2);
-			//graphics.refresh();
+			//graphics.fillRect(0, 0, (int) (1/11.0), 63);
+			RawColor color = RawColor.fromHTSensor(cs);
+			drawColor(new Rectangle(10, 10, 80, 32),color);
+//			int r = (int) (Math.pow(color.getR(),1/3.0)*80/maxScaled);
+//			int g = (int) (Math.pow(color.getG(),1/3.0)*80/maxScaled);
+//			int b = (int) (Math.pow(color.getB(),1/3.0)*80/maxScaled);
+//			graphics.fillRect(0, 60, r, 2);
+//			graphics.fillRect(0, 57, g, 2);
+//			graphics.fillRect(0, 54, b, 2);
+//			graphics.refresh();
 			Thread.sleep(50);
 		}
 	}
