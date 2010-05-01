a926330243c829cde858026c5880401e9c407564
Eric Wieser <wieser.eric@gmail.com>
2010-05-01 15:27:11 +0000

    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@81 57c2b024-eea2-457c-b231-805d8debf23e

---
+++ Robocup/Utils/trunk/NXTCam/src/lejos/nxt/addon/CamRect.java
@@ -0,0 +1,44 @@
+package lejos.nxt.addon;
+
+import java.awt.Rectangle;
+
+import lejos.nxt.addon.NXTCam;
+
+public class CamRect extends Rectangle
+{
+	public int colorId;
+
+	public CamRect(Rectangle rectangle, int objectColor)
+	{
+		super();
+		setRect(rectangle);
+		colorId = objectColor;
+	}
+
+	public int getArea()
+	{
+		return width * height;
+	}
+
+	public boolean intersects(Rectangle r)
+	{
+		final int tolerance = 3;
+		return super.intersects(new Rectangle(r.x - tolerance,
+		                                      r.y - tolerance,
+		                                      r.width + 2 * tolerance,
+		                                      r.height + 2 * tolerance));
+	}
+	
+	public static CamRect[] fromNXTCam(NXTCam camera)
+	{
+		int num = camera.getNumberOfObjects();
+		if(num <= 0)
+			return new CamRect[] {};
+		CamRect[] rects = new CamRect[num];
+		for(int i = 0; i < rects.length; i++)
+		{
+			rects[i] = new CamRect(camera.getRectangle(i), camera.getObjectColor(i));
+		}
+		return rects;
+	}
+}
