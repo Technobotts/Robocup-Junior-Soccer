46d160bbbc6c0cc2eed8bbb6f94cdc37c7f27f9d
Eric <Eric@57c2b024-eea2-457c-b231-805d8debf23e>
2010-05-15 15:52:14 +0000

    Moved all sensor abstractions into one project
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@114 57c2b024-eea2-457c-b231-805d8debf23e

---
+++ Robocup/Utils/trunk/Sensors/src/lejos/robotics/ImprovedRangeReadings.java
@@ -0,0 +1,39 @@
+package lejos.robotics;
+
+
+public class ImprovedRangeReadings extends RangeReadings
+{
+
+	public ImprovedRangeReadings(int numReadings)
+	{
+		super(numReadings);
+	}
+
+	public float getRange()
+	{
+		return getRangeReading().getRange();
+	}
+	public float getAngle()
+	{
+		return getRangeReading().getAngle();
+	}
+	
+	public RangeReading getRangeReading()
+	{
+		float x = 0;
+		float y = 0;
+		
+		for(RangeReading reading:this)
+		{
+			if(!reading.invalidReading())
+			{
+				x += reading.getRange()*Math.sin(Math.toRadians(reading.getAngle()));
+				y += reading.getRange()*Math.cos(Math.toRadians(reading.getAngle()));
+			}
+		}
+		
+		float range = (float) Math.sqrt(x*x+y*y);
+		float angle = (float) Math.toDegrees(Math.atan2(x, y));
+		return new RangeReading(angle, range);
+	}
+}
