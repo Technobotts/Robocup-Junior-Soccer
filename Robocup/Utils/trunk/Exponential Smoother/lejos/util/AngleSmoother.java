3fa8674e55f95f40a0c854159380239c0aa576b0
Eric Wieser <wieser.eric@gmail.com>
2010-05-29 16:28:14 +0000

    Added smoother for discontinous angles
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@141 57c2b024-eea2-457c-b231-805d8debf23e

---
+++ Robocup/Utils/trunk/Exponential Smoother/lejos/util/AngleSmoother.java
@@ -0,0 +1,26 @@
+package lejos.util;
+
+public class AngleSmoother extends Smoother
+{
+
+	public AngleSmoother(double t)
+	{
+		super(t);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setInput(double input)
+	{
+		double rawOutput = ((BasicSmoother) this).getOutput();
+		while(input - rawOutput > 180)
+			input -= 360;
+		while(input - rawOutput <= -180)
+			input += 360;
+
+		super.setInput(input);
+		((BasicSmoother) this).average = ((BasicSmoother) this).average%360;
+		super.average = super.average%360;
+	}
+
+}
