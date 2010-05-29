7e60b013b33ab039b43f64fdd8580bac2f9cfeef
Eric <Eric@57c2b024-eea2-457c-b231-805d8debf23e>
2010-05-29 15:21:22 +0000

    Calculates the angle for the robot to drive at for a given ball angle.
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@136 57c2b024-eea2-457c-b231-805d8debf23e

---
+++ Robocup/Soccer/trunk/New Robot/src/RobotAngleTest.java
@@ -0,0 +1,36 @@
+import technobotts.soccer.util.RobotDirectionModifier;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.SensorPort;
+import lejos.nxt.addon.CompassSensor;
+import lejos.nxt.addon.InvertedCompassSensor;
+import lejos.util.DataProcessor;
+
+
+public class RobotAngleTest
+{
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException
+	{
+		CompassSensor sensor = new InvertedCompassSensor(SensorPort.S1);
+		DataProcessor dp = new RobotDirectionModifier(1.5);
+		
+		while(!Button.ESCAPE.isPressed())
+		{
+			double angle = sensor.getDegreesCartesian();
+			double calc = dp.getOutput(angle);
+			
+			LCD.clear();
+			LCD.drawString("Raw:  "+angle, 0, 0);
+			LCD.drawString("Calc: "+calc, 0, 1);
+			LCD.refresh();
+			
+			Thread.sleep(100);
+		}
+	}
+
+}
