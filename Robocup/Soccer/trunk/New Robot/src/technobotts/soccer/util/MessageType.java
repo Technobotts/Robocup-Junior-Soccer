fd73436e68ac4c94cee9ee0f43155b4ea128a974
Eric <Eric@57c2b024-eea2-457c-b231-805d8debf23e>
2010-06-12 11:49:18 +0000

    Changed to work with bumper
    git-svn-id: http://technobotts.svn.beanstalkapp.com/all@170 57c2b024-eea2-457c-b231-805d8debf23e

--- Robocup/Soccer/trunk/New Robot/src/technobotts/soccer/util/MessageType.java
+++ Robocup/Soccer/trunk/New Robot/src/technobotts/soccer/util/MessageType.java
@@ -1,10 +1,11 @@
 package technobotts.soccer.util;
 
 public enum MessageType {
-	KICK     (0x01),
-	US_PING  (0x02),
-	GOAL_POS (0x04),
-	SHUTDOWN (0x08);
+	KICK(0x01),
+	US_PING(0x02),
+	BUMPER_CHECK(0x02),
+	GOAL_POS(0x08),
+	SHUTDOWN(0x10);
 
 	private byte value;
 
